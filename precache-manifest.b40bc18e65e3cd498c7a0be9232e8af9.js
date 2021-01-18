ff1246b8797373329083f3a356adb614affb73d3
Veronika Dodda <v.shengeliya@gmail.com>
2021-01-18 18:08:59 -0500

    Updates

---
+++ precache-manifest.b40bc18e65e3cd498c7a0be9232e8af9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb65668e462ea2931e8acd337f8729fa",
+    "url": "/new_repo/index.html"
+  },
+  {
+    "revision": "f21045efd238bb2d074b",
+    "url": "/new_repo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f8373a438c6aa6054957",
+    "url": "/new_repo/static/js/2.14e6cfe8.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/new_repo/static/js/2.14e6cfe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62eafffb1071b11b0c1c",
+    "url": "/new_repo/static/js/3.30d41a45.chunk.js"
+  },
+  {
+    "revision": "f21045efd238bb2d074b",
+    "url": "/new_repo/static/js/main.64d8febe.chunk.js"
+  },
+  {
+    "revision": "08f77910909635ca9a4b",
+    "url": "/new_repo/static/js/runtime-main.626aecf3.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/new_repo/static/media/logo.06e73328.svg"
+  }
+]);
